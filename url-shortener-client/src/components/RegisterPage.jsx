996ec055ab19f5afea4f8e3b82ff202205c8ecf6
Facundo Luna <facundolunaok@gmail.com>
2025-05-02 23:59:12 -0300

    WIP: user registration API integration.

--- url-shortener-client/src/components/RegisterPage.jsx
+++ url-shortener-client/src/components/RegisterPage.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import TextField from './TextField';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const RegisterPage = () => {
     const [loader, setLoader] = useState(false);
+    const navigate = useNavigate();
 
     const {
         register,
@@ -21,7 +23,22 @@ const RegisterPage = () => {
     });
 
     const registerHandler = async (data) => {
+        setLoader(true);
 
+        try {
+            const { data: response } = await api.post(
+                "/api/auth/public/register",
+                data
+            );
+            reset();
+            navigate("/login");
+            toast.success("Registration Successfull!");
+        } catch (error) {
+            console.log(error);
+            toast.error("Registration Failed!")
+        } finally {
+            setLoader(false);
+        }
     };
 
     return (
