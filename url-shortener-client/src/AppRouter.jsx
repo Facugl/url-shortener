47605a7e1710324673c74e03eb97c36a0bdcbbcb
Facundo Luna <facundolunaok@gmail.com>
2025-05-11 16:37:41 -0300

    build(frontend): using path based routing for short urls.

--- url-shortener-client/src/AppRouter.jsx
+++ url-shortener-client/src/AppRouter.jsx
@@ -13,20 +13,22 @@ import PrivateRoute from './PrivateRoute'
 import ErrorPage from './components/ErrorPage'
 
 const AppRouter = () => {
+    const hideHeaderFooter = location.pathname.startsWith("/s");
     return (
         <>
-            <Navbar />
+            {!hideHeaderFooter && <Navbar />}
             <Toaster position='bottom-center' />
             <Routes>
                 <Route path='/' element={<LandingPage />} />
                 <Route path='/about' element={<AboutPage />} />
+                <Route path='/s/:url' element={<ShortenUrlPage />} />
                 <Route path='/register' element={<PrivateRoute publicPage={true}><RegisterPage /></PrivateRoute>} />
                 <Route path='/login' element={<PrivateRoute publicPage={true}><LoginPage /></PrivateRoute>} />
                 <Route path='/dashboard' element={<PrivateRoute publicPage={false}><DashboardLayout /></PrivateRoute>} />
                 <Route path='/error' element={<ErrorPage />} />
                 <Route path='*' element={<ErrorPage message="We can't seem to find the page you're looking for" />} />
             </Routes>
-            <Footer />
+            {!hideHeaderFooter && <Footer />}
         </>
     )
 }
--- url-shortener-client/src/components/ShortenUrlPage.jsx
+++ url-shortener-client/src/components/ShortenUrlPage.jsx
@@ -2,14 +2,14 @@ import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const ShortenUrlPage = () => {
-    const {url} = useParams();
+    const { url } = useParams();
 
     useEffect(() => {
-        if  (url) {
+        if (url) {
             window.location.href = import.meta.env.VITE_BACKEND_URL + `/${url}`;
         }
     }, [url])
-  return null;
+    return <p>Redirecting...</p>;
 }
 
 export default ShortenUrlPage
